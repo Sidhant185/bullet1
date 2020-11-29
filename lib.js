eacb69f1d6500c435792b838411d68789b0b33b3
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2020-11-29 15:14:18 +0530

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,9 @@
+function collide(lbullet,lwall){
+lbulletRightEdge = lbullet.x+lbullet.width;
+lwallLeftEgde = lwall.x
+if (bulletRightEdge >=walLeftEdge){
+    return  true;
+}else{
+    return false;
+}
+}
